ec1c69980c40d798b371fed0bef1ca78d1f5ff08
Anar Bayramov <anar.bayram@yahoo.com>
2024-08-21 12:03:46 +0400

    refactor: Rename folder includes files

--- feedbacks/constants.py
+++ feedbacks/constants.py
@@ -1,3 +1,3 @@
 FEEDBACK_COLLECTION_NAME = "feedbacks"
-PIPELINE_FILE_RELATIVE_PATH  = "../data/pipelines.json"
-SEEDING_FILE_RELATIVE_PATH = "../data/feedback.json"
+PIPELINE_FILE_RELATIVE_PATH  = "../files/pipelines.json"
+SEEDING_FILE_RELATIVE_PATH = "../files/feedback.json"
